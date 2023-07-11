4eb8f9abd5531641ca5753b1f5adb8a71a2b1d29
E3Man <130256824+E3Man@users.noreply.github.com>
2023-07-10 20:34:47 -0600

    Added 3 Chat System methods for V4.0

--- sryox_sents_funeware/lua/autorun/server/sh_syrox_npc_methods.lua
+++ sryox_sents_funeware/lua/autorun/server/sh_syrox_npc_methods.lua
@@ -17,3 +17,32 @@ function NPCJumpRand(npc, right, left, height)
         npc:MoveJumpStop() 
     end)
 end
+
+function NPCChat(npcname, message, plys)
+    local final = npcname .. ": " .. message
+    plys:ChatPrint(final)
+end
+
+function NPCChatAdv(npcname, message, range, pos)
+    local plys = ents.FindInSphere(pos:GetPos(), range)
+    for _, v in pairs(plys) do
+        if v:IsPlayer() then
+            NPCChat(npcname, message, v)
+        end
+    end
+end
+
+function NPCChatAdvRand(npcname, message, range, pos, bool)
+    local plys = ents.FindInSphere(pos:GetPos(), range)
+    for _, v in pairs(plys) do
+    if bool == false then
+        if v:IsPlayer() then
+            NPCChat(npcname, message[math.random(1, #message)], v)
+        end
+    elseif bool == true then 
+     if v:IsPlayer() then
+            NPCChat(npcname, "<::"..message[math.random(1, #message)].."::>", v)
+        end
+    end
+end
+end
